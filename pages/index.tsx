df2b22402b4219345226bb7009355e32bfc9d9b9
dag-bag <virenderkumar23435@gmail.com>
2022-06-09 17:50:25 +0530

    main

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ import Link from "next/link";
 interface Props {
   posts: Post[];
 }
-const Home: NextPage<Props> = ({ posts }) => {
+const Home: NextPage = ({ posts }: any) => {
   return (
     <div>
       <Head>
